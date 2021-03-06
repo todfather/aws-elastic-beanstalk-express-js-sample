ea38c4404d5507cad191be5672a36b188f732a94
todfather <todfather@gmail.com>
2021-03-06 10:52:10 -0500

    Update Message 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Lucy and Matthew!'));
+app.get('/', (req, res) => res.send('Hello Lucy and Matthew!\nHow Cool is this?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
