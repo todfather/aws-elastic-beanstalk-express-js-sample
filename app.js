1267c1b85a601dc7fd18c6a68bc8883974f19e47
todfather <todfather@gmail.com>
2021-03-06 11:01:37 -0500

    Update Message 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Lucy and Matthew!\nHow Cool is this?'));
+app.get('/', (req, res) => res.send('Hello Lucy and Matthew! How Cool is this? I do not know Javascript that well'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
